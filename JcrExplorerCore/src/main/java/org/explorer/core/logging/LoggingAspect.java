ddd581ffdba3610c2b7016f48252d91ed5cd2ec6
mkhan <mehmoods2u@gmail.com>
2014-06-30 23:41:46 -0400

    pushing the aspects for logging

--- JcrExplorerCore/src/main/java/org/explorer/core/logging/LoggingAspect.java
+++ JcrExplorerCore/src/main/java/org/explorer/core/logging/LoggingAspect.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 	private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 	
-	@Around("execution(* org.explorer..*.*(..))")
+	@Around("execution(* *(..))")
 	public Object log(ProceedingJoinPoint joinPoint) throws Throwable{
 		Object returnVal=joinPoint.proceed();
 		StringBuffer logMsg=new StringBuffer();
